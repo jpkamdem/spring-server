641ffd9bbacec140af8ff34405b736c4a7767186
jpkam <jpkamdem@proton.me>
2025-10-01 16:56:05 +0200

    commit initial

---
+++ src/test/java/konnnro/backend/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package konnnro.backend.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
